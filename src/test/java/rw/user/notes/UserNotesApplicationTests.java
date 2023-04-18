e2d936106b5d4da9ccc7c324fea0509ba62088d0
mazimpakarichard <mazimpaka51@gmail.com>
2023-04-18 11:45:33 +0200

    initial commit

---
+++ src/test/java/rw/user/notes/UserNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.user.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
